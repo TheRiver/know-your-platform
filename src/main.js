27338484ae96fb8748d4bf8077d4823768c8f350
Constance Neeser <TheRiver@users.noreply.github.com>
2022-10-28 12:20:13 +0200

    Added #getBrowser, which returns a string representing the browser name

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
  * @returns {boolean}
  */
 export function isChrome(user_agent) {
-    return user_agent.indexOf("Chrome") > -1;
+    return user_agent.indexOf("Chrome") > -1 && !isEdge(user_agent);
 }
 
 export function isFirefox(user_agent) {
@@ -12,10 +12,18 @@ export function isFirefox(user_agent) {
 }
 
 export function isSafari(user_agent) {
-    return user_agent.indexOf("Safari") > -1 && !isChrome(user_agent);
+    return user_agent.indexOf("Safari") > -1 && !isChrome(user_agent) && !isEdge(user_agent);
 }
 
 export function isEdge(user_agent) {
     return user_agent.indexOf("Edg") > -1;
 }
 
+export function getBrowser(user_agent) {
+    if (isChrome(user_agent)) return "CHROME";
+    if (isFirefox(user_agent)) return "FIREFOX";
+    if (isSafari(user_agent)) return "SAFARI";
+    if (isEdge(user_agent)) return "EDGE";
+
+    return "OTHER";
+}
