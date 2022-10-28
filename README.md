2eefb083229d1cfccb4da2bf7633627377ebdc32
Constance Neeser <TheRiver@users.noreply.github.com>
2022-10-28 10:42:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Know your Platform
+
+This is a very simple library to enable you to test what browser your code is running in. This is meant as a way to understand your userbase, and *NOT* as a way to write browser specific code. You should be using feature detection, polyfills, and @support queries if that is your use case. 
+
+
+## License
+
+This software is distributed under the MIT license. See the [license](/LICENSE) file for a copy. 
---
+++ src/main.js
@@ -0,0 +1,20 @@
+/**
+ * Returns true if the given user agent is from chrome. 
+ * @param {string} user_agent 
+ * @returns {boolean}
+ */
+export function isChrome(user_agent) {
+    return user_agent.indexOf("Chrome") > -1;
+}
+
+export function isFirefox(user_agent) {
+    return user_agent.indexOf("Firefox") > -1;
+}
+
+export function isSafari(user_agent) {
+    return user_agent.indexOf("Safari") > -1 && !isChrome(user_agent);
+}
+
+export function isEdge(user_agent) {
+    return user_agent.indexOf("Edg") > -1;
+}
